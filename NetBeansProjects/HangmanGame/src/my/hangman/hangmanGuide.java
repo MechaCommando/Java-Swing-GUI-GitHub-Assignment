53924a82f81c85c9c6e7366f76bcfbc727e639e5
hzhan1 <hzhan1@LDHS-BBT-148141.ad.ocdsb.ca>
2024-03-27 11:07:57 -0400

    fourth

--- NetBeansProjects/HangmanGame/src/my/hangman/hangmanGuide.java
+++ NetBeansProjects/HangmanGame/src/my/hangman/hangmanGuide.java
@@ -91,8 +91,8 @@ public class hangmanGuide extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        hangmanUI pi = new hangmanUI();
-        pi.show();
+        //hangmanUI pi = new hangmanUI();
+        //pi.show();
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
