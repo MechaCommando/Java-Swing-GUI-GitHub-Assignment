e3ba604fb615737c1977b87ca52499fb5ac83302
Yahya <Yahya@DESKTOP-35TB8EH.ht.home>
2024-04-02 18:35:50 -0400

    minor changes (Scoreboard and LOSE/WIN Screens missing)

--- NetBeansProjects/HangmanGame/src/my/hangman/hangmanScoreboard.java
+++ NetBeansProjects/HangmanGame/src/my/hangman/hangmanScoreboard.java
@@ -33,10 +33,11 @@ public class hangmanScoreboard extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        backToGameButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("BACK");
+        jButton1.setText("BACK TO MAIN");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -76,18 +77,27 @@ public class hangmanScoreboard extends javax.swing.JFrame {
             jTable1.getColumnModel().getColumn(0).setResizable(false);
         }
 
+        backToGameButton.setText("BACK TO GAME");
+        backToGameButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backToGameButtonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(413, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(backToGameButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
                 .addGap(17, 17, 17)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(99, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -95,7 +105,9 @@ public class hangmanScoreboard extends javax.swing.JFrame {
                 .addContainerGap(11, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(backToGameButton))
                 .addContainerGap())
         );
 
@@ -114,7 +126,7 @@ public class hangmanScoreboard extends javax.swing.JFrame {
     
     public static void addToScoreboard(String username) {
     if (tableModel != null) {
-        tableModel.addRow(new String[]{username});
+       tableModel.addRow(new String[]{username});
     } else {
         // Initialize the components if not already initialized
         getInstance().initComponents();
@@ -132,6 +144,13 @@ public class hangmanScoreboard extends javax.swing.JFrame {
         dispose();
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void backToGameButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backToGameButtonActionPerformed
+        mainHangmanUI newframe = new mainHangmanUI();
+        newframe.setVisible(true);
+        newframe.toFront();
+        dispose();
+    }//GEN-LAST:event_backToGameButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -170,6 +189,7 @@ public class hangmanScoreboard extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton backToGameButton;
     private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
